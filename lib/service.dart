65532b37533ba02f1cae1ff34facf6dbca577b70
Abelboby <abeldavisboby@gmail.com>
2025-01-11 11:43:18 +0530

    Update:try and catch in apiservice

--- lib/service.dart
+++ lib/service.dart
@@ -7,19 +7,9 @@ class Service {
     try {
       final response = await http.get(Uri.parse(
           'https://2fa0d036-25f8-4bc9-80a4-ff1726e4e097.mock.pstmn.io/caddayn/mock/users/$userId'));
-      if (response.statusCode == 200) {
         return ActorModel.fromJson(jsonDecode(response.body));
-      } else {
-        final decodedResponse = jsonDecode(response.body);
-        if (decodedResponse['data'] == null) {
-          throw 'User not found';
-        }
-        else {
-          throw 'Something went wrong';
-        }
-      }
     } catch (e) {
-      throw e.toString();
+      throw "User not found";
     }
   }
 }
