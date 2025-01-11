404fff7b5b1973727562595c8e9cde7cda71802b
Abelboby <abeldavisboby@gmail.com>
2025-01-11 11:03:59 +0530

    Update: meaningful varibales for model

--- lib/model.dart
+++ lib/model.dart
@@ -1,81 +1,82 @@
+
 import 'dart:convert';
 
-Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+ActorModel actorModelFromJson(String str) => ActorModel.fromJson(json.decode(str));
 
-String welcomeToJson(Welcome data) => json.encode(data.toJson());
+String actorModelToJson(ActorModel data) => json.encode(data.toJson());
 
-class Welcome {
-  bool success;
-  Data data;
-  dynamic error;
+class ActorModel {
+    bool success;
+    Data data;
+    dynamic error;
 
-  Welcome({
-    required this.success,
-    required this.data,
-    required this.error,
-  });
+    ActorModel({
+        required this.success,
+        required this.data, 
+        required this.error,
+    });
 
-  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
-    success: json["success"],
-    data: Data.fromJson(json["data"]),
-    error: json["error"],
-  );
+    factory ActorModel.fromJson(Map<String, dynamic> json) => ActorModel(
+        success: json["success"],
+        data: Data.fromJson(json["data"]),
+        error: json["error"],
+    );
 
-  Map<String, dynamic> toJson() => {
-    "success": success,
-    "data": data.toJson(),
-    "error": error,
-  };
+    Map<String, dynamic> toJson() => {
+        "success": success,
+        "data": data.toJson(),
+        "error": error,
+    };
 }
 
 class Data {
-  User user;
-  String message;
+    User user;
+    String message;
 
-  Data({
-    required this.user,
-    required this.message,
-  });
+    Data({
+        required this.user,
+        required this.message,
+    });
 
-  factory Data.fromJson(Map<String, dynamic> json) => Data(
-    user: User.fromJson(json["user"]),
-    message: json["message"],
-  );
+    factory Data.fromJson(Map<String, dynamic> json) => Data(
+        user: User.fromJson(json["user"]),
+        message: json["message"],
+    );
 
-  Map<String, dynamic> toJson() => {
-    "user": user.toJson(),
-    "message": message,
-  };
+    Map<String, dynamic> toJson() => {
+        "user": user.toJson(),
+        "message": message,
+    };
 }
 
 class User {
-  int userId;
-  String name;
-  int age;
-  String profession;
-  String profileImage;
+    int userId;
+    String name;
+    int age;
+    String profession;
+    String profileImage;
 
-  User({
-    required this.userId,
-    required this.name,
-    required this.age,
-    required this.profession,
-    required this.profileImage,
-  });
+    User({
+        required this.userId,
+        required this.name,
+        required this.age,
+        required this.profession,
+        required this.profileImage,
+    });
 
-  factory User.fromJson(Map<String, dynamic> json) => User(
-    userId: json["user_id"],
-    name: json["name"],
-    age: json["age"],
-    profession: json["profession"],
-    profileImage: json["profile_image"],
-  );
+    factory User.fromJson(Map<String, dynamic> json) => User(
+        userId: json["user_id"],
+        name: json["name"],
+        age: json["age"],
+        profession: json["profession"],
+        profileImage: json["profile_image"],
+    );
 
-  Map<String, dynamic> toJson() => {
-    "user_id": userId,
-    "name": name,
-    "age": age,
-    "profession": profession,
-    "profile_image": profileImage,
-  };
+    Map<String, dynamic> toJson() => {
+        "user_id": userId,
+        "name": name,
+        "age": age,
+        "profession": profession,
+        "profile_image": profileImage,
+    };
 }
--- lib/service.dart
+++ lib/service.dart
@@ -3,11 +3,11 @@ import 'model.dart';
 import 'dart:convert';
 
 class Service {
-  Future<Welcome> getUserInfo(String userId) async {
+  Future<ActorModel> getUserInfo(String userId) async {
     final response = await http.get(Uri.parse(
         'https://2fa0d036-25f8-4bc9-80a4-ff1726e4e097.mock.pstmn.io/caddayn/mock/users/$userId'));
     if (response.statusCode == 200) {
-      return Welcome.fromJson(jsonDecode(response.body));
+      return ActorModel.fromJson(jsonDecode(response.body));
     } else {
       final decodedResponse = jsonDecode(response.body);
       throw decodedResponse['error'] ?? 'Something went wrong';
