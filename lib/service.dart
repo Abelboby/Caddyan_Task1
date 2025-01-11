b72c7e26dd06e2ffdb63a19e5c60e181033cef56
Abelboby <abeldavisboby@gmail.com>
2025-01-11 21:58:50 +0530

    Update: Added connection checker

--- lib/service.dart
+++ lib/service.dart
@@ -1,19 +1,25 @@
 import 'package:http/http.dart' as http;
-import 'model.dart';
 import 'dart:convert';
+import 'dart:io';
+import 'model.dart';
 
 class Service {
   Future<ActorModel> getUserInfo(String userId) async {
-    final response = await http.get(Uri.parse(
-        'https://2fa0d036-25f8-4bc9-80a4-ff1726e4e097.mock.pstmn.io/caddayn/mock/users/$userId'));
-
-    if (response.statusCode == 404) {
-      throw 'User not found';
-    }
     try {
-      return ActorModel.fromJson(jsonDecode(response.body));
+      final response = await http.get(Uri.parse(
+          'https://2fa0d036-25f8-4bc9-80a4-ff1726e4e097.mock.pstmn.io/caddayn/mock/users/$userId'));
+      final decodedResponse = jsonDecode(response.body);
+      return ActorModel.fromJson(decodedResponse);
+    } on SocketException {
+      throw 'No internet connection. Please check your network.';
     } catch (e) {
-      throw 'Something went wrong';
+      // print(e);
+      // if (e.toString().contains('No user found')) {
+      //   throw 'User not found';
+      // } else {
+        throw 'Something went wrong.';
+      //}
     }
   }
 }
+
