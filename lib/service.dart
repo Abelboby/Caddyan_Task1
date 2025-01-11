6b8cc9735aa5558281f86b7458472c5bb5b3aab7
Abelboby <abeldavisboby@gmail.com>
2025-01-11 11:40:07 +0530

    Update:try catch in apiresponse and meaningful messages added

--- lib/service.dart
+++ lib/service.dart
@@ -4,13 +4,22 @@ import 'dart:convert';
 
 class Service {
   Future<ActorModel> getUserInfo(String userId) async {
-    final response = await http.get(Uri.parse(
-        'https://2fa0d036-25f8-4bc9-80a4-ff1726e4e097.mock.pstmn.io/caddayn/mock/users/$userId'));
-    if (response.statusCode == 200) {
-      return ActorModel.fromJson(jsonDecode(response.body));
-    } else {
-      final decodedResponse = jsonDecode(response.body);
-      throw decodedResponse['error'] ?? 'Something went wrong';
+    try {
+      final response = await http.get(Uri.parse(
+          'https://2fa0d036-25f8-4bc9-80a4-ff1726e4e097.mock.pstmn.io/caddayn/mock/users/$userId'));
+      if (response.statusCode == 200) {
+        return ActorModel.fromJson(jsonDecode(response.body));
+      } else {
+        final decodedResponse = jsonDecode(response.body);
+        if (decodedResponse['data'] == null) {
+          throw 'User not found';
+        }
+        else {
+          throw 'Something went wrong';
+        }
+      }
+    } catch (e) {
+      throw e.toString();
     }
   }
 }
