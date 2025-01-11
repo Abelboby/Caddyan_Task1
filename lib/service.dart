c82f97a1e43af2ef6b9a54b9dd5bd5f554a7b601
Abelboby <abeldavisboby@gmail.com>
2025-01-11 11:56:34 +0530

    Update:added user not found

--- lib/service.dart
+++ lib/service.dart
@@ -4,9 +4,13 @@ import 'dart:convert';
 
 class Service {
   Future<ActorModel> getUserInfo(String userId) async {
+    final response = await http.get(Uri.parse(
+        'https://2fa0d036-25f8-4bc9-80a4-ff1726e4e097.mock.pstmn.io/caddayn/mock/users/$userId'));
+
+    if (response.statusCode == 404) {
+      throw 'User not found';
+    }
     try {
-      final response = await http.get(Uri.parse(
-          'https://2fa0d036-25f8-4bc9-80a4-ff1726e4e097.mock.pstmn.io/caddayn/mock/users/$userId'));
       return ActorModel.fromJson(jsonDecode(response.body));
     } catch (e) {
       throw 'Something went wrong';
