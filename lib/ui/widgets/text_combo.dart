ee6bcfc0589629cba5d308fe033e92e148dd7101
Abelboby <abeldavisboby@gmail.com>
2025-01-14 20:45:52 +0530

    Ignore: Reformated code

--- lib/ui/widgets/text_combo.dart
+++ lib/ui/widgets/text_combo.dart
@@ -19,23 +19,14 @@ class TextComboWidget extends StatelessWidget {
           width: 75,
           child: Text(
             label,
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w600,
-            ),
+            style: const TextStyle(color: Colors.white,fontWeight: FontWeight.w600),
           ),
         ),
-        const Text(
-          ":",
-          style: TextStyle(color: Colors.white),
-        ),
+        const Text(":",style: TextStyle(color: Colors.white),),
         const SizedBox(width: 8),
         Text(
           value,
-          style: const TextStyle(
-            color: Colors.yellow,
-            fontWeight: FontWeight.w600,
-          ),
+          style: const TextStyle(color: Colors.yellow,fontWeight: FontWeight.w600),
         ),
       ],
     );
